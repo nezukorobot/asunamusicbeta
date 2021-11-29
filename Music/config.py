1472b1bfdab0a70d53a502c7b0d02e316baed0f4
apurbatg <93421564+apurbatg@users.noreply.github.com>
2021-11-29 21:50:08 +0530

    Yep

---
+++ Music/config.py
@@ -0,0 +1,17 @@
+##Config
+
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+SESSION_NAME = getenv('SESSION_NAME', 'session')
+BOT_TOKEN = getenv('BOT_TOKEN')
+API_ID = int(getenv('API_ID', "6435225"))
+API_HASH = getenv('API_HASH', '4e984ea35f854762dcde906dce426c2d')
+DURATION_LIMIT = int(getenv('DURATION_LIMIT', '540000'))
+COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ ! .').split())
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://TGN:TGN@cluster0.c4yk3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+SUDO_USERS = list(map(int, getenv('SUDO_USERS', '1021022522').split()))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", '-1001436232299'))
+ASS_ID = int(getenv("ASS_ID", '2042279052'))
+OWNER_ID = list(map(int, getenv('OWNER_ID', '1766781111').split()))
